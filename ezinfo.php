8730cea983b1b59f77ef8c95deb37b78e37b700e
gg <gg@8ae3b124-d6c5-461a-b96a-ed4a83a48eb2>
2009-07-29 10:29:07 +0000

    - update version number for next release
    git-svn-id: http://svn.projects.ez.no/ezdebug_template_operator/trunk/extension/ezdebug@4 8ae3b124-d6c5-461a-b96a-ed4a83a48eb2

--- ezinfo.php
+++ ezinfo.php
@@ -5,7 +5,7 @@ class ezdebugInfo
     {
         return array(
             'Name' => "eZ Debug extension",
-            'Version' => "0.1",
+            'Version' => "0.2",
             'Copyright' => "Copyright (C) 2008 G. Giunta",
             'License' => "GNU General Public License v2.0"
         );
