a946626980a34bf1084d9b1c4939714a310f17b6
gg <gg@8ae3b124-d6c5-461a-b96a-ed4a83a48eb2>
2010-10-12 22:15:05 +0000

    - add ajax call to inspect stuff (server side)
    git-svn-id: http://svn.projects.ez.no/ezdebug_template_operator/trunk/extension/ezdebug@25 8ae3b124-d6c5-461a-b96a-ed4a83a48eb2

---
+++ settings/ezjscore.ini.append.php
@@ -0,0 +1,16 @@
+<?php /*
+
+[ezjscServer]
+FunctionList[]=ezdebug_inspect
+FunctionList[]=ezstarrating_user_has_rated
+
+[ezjscServer_ezstarrating]
+# Url to test this server function(rate):
+# <root>/ezjscore/call/ezdebug::inspect::<...to_be_determined...>
+Class=ezdebugJSCFunctions
+# Policies
+Functions[]=ezdebug
+PermissionPrFunction=enabled
+#File=extension/ezstarrating/classes/ezsrserverfunctions.php
+
+*/ ?>
