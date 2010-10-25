07faafd1c6c4a13159d3788be0fcc5aa443dbc82
gg <gg@8ae3b124-d6c5-461a-b96a-ed4a83a48eb2>
2010-10-25 00:10:53 +0000

    - add declaration of included sw in ezinfo.php
    git-svn-id: http://svn.projects.ez.no/ezdebug_template_operator/trunk/extension/ezdebug@38 8ae3b124-d6c5-461a-b96a-ed4a83a48eb2

--- ezinfo.php
+++ ezinfo.php
@@ -6,8 +6,13 @@ class ezdebugInfo
         return array(
             'Name' => "eZ Debug extension",
             'Version' => "0.4-beta",
-            'Copyright' => "Copyright (C) 2008-2010 G. Giunta",
-            'License' => "GNU General Public License v2.0"
+            'Copyright' => "Copyright (C) 2008-2010 Gaetano Giunta",
+            'License' => "GNU General Public License v2.0",
+            'Includes the following third-party software' => array(
+                'Name' => 'YUI',
+                'Version' => "2.5.0",
+                'Copyright' => 'Copyright (c) 2010, Yahoo! Inc. All rights reserved.',
+                'License' => 'Licensed under the BSD License' )
         );
     }
 }
