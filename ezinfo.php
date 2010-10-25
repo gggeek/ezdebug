06f97e261aa30f8ab04b64124ea23ad76d8a5d93
gg <gg@8ae3b124-d6c5-461a-b96a-ed4a83a48eb2>
2010-10-25 00:19:02 +0000

    - update info file
    git-svn-id: http://svn.projects.ez.no/ezdebug_template_operator/trunk/extension/ezdebug@39 8ae3b124-d6c5-461a-b96a-ed4a83a48eb2

--- ezinfo.php
+++ ezinfo.php
@@ -4,12 +4,12 @@ class ezdebugInfo
     static function info()
     {
         return array(
-            'Name' => "eZ Debug extension",
+            'Name' => "<a href=\"http://projects.ez.no/ezdebug_template_operator\">eZ Debug extension</a>",
             'Version' => "0.4-beta",
             'Copyright' => "Copyright (C) 2008-2010 Gaetano Giunta",
             'License' => "GNU General Public License v2.0",
             'Includes the following third-party software' => array(
-                'Name' => 'YUI',
+                'Name' => '<a href="http://developer.yahoo.com/yui\">YUI</a>',
                 'Version' => "2.5.0",
                 'Copyright' => 'Copyright (c) 2010, Yahoo! Inc. All rights reserved.',
                 'License' => 'Licensed under the BSD License' )
